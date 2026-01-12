ef435e30f23d9db0570d608cefad2f20dd106d19
Andrew Drive <86859259+Andrew-Drive@users.noreply.github.com>
2026-01-12 13:48:13 -0800

    Updated API logic

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
-  cron.schedule('*/5 * * * *', checkForNewVideos); // Every 5 mins - a fair warning that this overloads YouTube API limits
+  cron.schedule('*/15 * * * *', checkForNewVideos); // Every 15 mins to save quota
 });
 
 
@@ -36,29 +36,91 @@ client.on('interactionCreate', async interaction => {
 });
 
 async function checkForNewVideos() {
+  const channelIds = new Set();
   for (const guildId in trackedChannels) {
     for (const channelId in trackedChannels[guildId].youtubeChannels) {
-      const lastVideoId = trackedChannels[guildId].youtubeChannels[channelId];
-      const newVideo = await getLatestVideo(channelId);
+      channelIds.add(channelId);
+    }
+  }
 
-      if (newVideo && newVideo.id.videoId !== lastVideoId) {
-        const discordChannel = await client.channels.fetch(trackedChannels[guildId].discordChannelId);
-        if (discordChannel) {
-          await discordChannel.send(`<@everyone> 📢 New YouTube video from ${newVideo.snippet.channelTitle}! Feel free to check it out!\nhttps://youtu.be/${newVideo.id.videoId}`);
+  if (channelIds.size === 0) return;
+
+  // Process in batches of 50 (API limit)
+  const channelList = Array.from(channelIds);
+  for (let i = 0; i < channelList.length; i += 50) {
+    const batch = channelList.slice(i, i + 50);
+    const uploadsData = await getUploadsBatch(batch);
+    
+    for (const guildId in trackedChannels) {
+      for (const channelId of batch) {
+        if (!trackedChannels[guildId].youtubeChannels[channelId]) continue;
+        
+        const lastVideoId = trackedChannels[guildId].youtubeChannels[channelId];
+        const latestVideo = uploadsData[channelId];
+
+        if (latestVideo && latestVideo.videoId !== lastVideoId) {
+          const discordChannelId = trackedChannels[guildId].discordChannelId;
+          
+          if (!discordChannelId) {
+            console.error(`No alert channel set for guild ${guildId}`);
+            continue;
+          }
+
+          const discordChannel = await client.channels.fetch(discordChannelId).catch(() => null);
+          if (discordChannel) {
+            await discordChannel.send(`<@everyone> 📢 New YouTube video from ${latestVideo.channelTitle}! Feel free to check it out!\nhttps://youtu.be/${latestVideo.videoId}`).catch(console.error);
+          } else {
+            console.error(`Could not fetch alert channel ${discordChannelId} for guild ${guildId}. It may have been deleted.`);
+          }
+          trackedChannels[guildId].youtubeChannels[channelId] = latestVideo.videoId;
         }
-
-        trackedChannels[guildId].youtubeChannels[channelId] = newVideo.id.videoId;
-        fs.writeFileSync('channels.json', JSON.stringify(trackedChannels, null, 2));
       }
     }
+  }
+  fs.writeFileSync('channels.json', JSON.stringify(trackedChannels, null, 2));
+}
+
+async function getUploadsBatch(channelIds) {
+  try {
+    const results = {};
+    
+    for (const channelId of channelIds) {
+      const activitiesUrl = `https://www.googleapis.com/youtube/v3/activities?key=${process.env.YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,contentDetails&maxResults=5&type=upload`;
+      const activitiesRes = await axios.get(activitiesUrl);
+      
+      if (activitiesRes.data.items && activitiesRes.data.items.length > 0) {
+        // Find the most recent upload activity
+        const latestUpload = activitiesRes.data.items.find(item => item.snippet.type === 'upload');
+        if (latestUpload) {
+          results[channelId] = {
+            videoId: latestUpload.contentDetails.upload.videoId,
+            channelTitle: latestUpload.snippet.channelTitle
+          };
+        }
+      }
+    }
+    return results;
+  } catch (err) {
+    console.error('YouTube API batch error:', err.message);
+    return {};
   }
 }
 
 async function getLatestVideo(channelId) {
-  const url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=1&type=video`;
   try {
-    const res = await axios.get(url);
-    return res.data.items[0];
+    const activitiesUrl = `https://www.googleapis.com/youtube/v3/activities?key=${process.env.YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,contentDetails&maxResults=1&type=upload`;
+    const res = await axios.get(activitiesUrl);
+    
+    if (res.data.items && res.data.items.length > 0) {
+      const latestUpload = res.data.items[0];
+      if (latestUpload && latestUpload.contentDetails.upload) {
+        return {
+          id: { videoId: latestUpload.contentDetails.upload.videoId },
+          snippet: { channelTitle: latestUpload.snippet.channelTitle }
+        };
+      }
+    }
+    return null;
   } catch (err) {
     console.error('YouTube API error:', err.message);
     return null;
@@ -66,3 +128,4 @@ async function getLatestVideo(channelId) {
 }
 
 client.login(process.env.DISCORD_TOKEN);
+
