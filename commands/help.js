9c20b31c4225880e8bd0ec1bcf4e35b3d0015d8d
Andrew Drive <86859259+Andrew-Drive@users.noreply.github.com>
2026-02-05 17:51:18 -0800

    Add optional step for user role ping in help command

--- commands/help.js
+++ commands/help.js
@@ -12,6 +12,7 @@ module.exports = {
       .addFields(
         { name: 'Step 1: Set Alert Channel', value: 'Use `/setalertchannel` to choose which Discord channel will receive the notifications.' },
         { name: 'Step 2: Add YouTube Channel', value: 'Use `/addchannel` with the YouTube Channel ID to start tracking. (Example: `UC_x5XG1OV2P6uYZ5ujfW30A`)' },
+        { name: 'Optional Step: Choose role to ping yor users', value: 'Use `/setpingrole` with the Discord Server role you like to use for alerts. (Default role is @everyone)' },
         { name: 'Manage Tracking', value: '• `/listchannels`: See all YouTube channels currently being tracked.\n• `/removechannel`: Stop tracking a specific YouTube channel.' },
         { name: 'Permissions', value: 'Only users with **Administrator or Manage Server** permissions and server owners can use these commands.' }
       )
